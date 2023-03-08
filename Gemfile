21ed80328cdb663a14788c755daeca0dc8444d2e
revathi <revathi8104@gmail.com>
2023-03-08 15:45:29 +0530

    second commit

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem 'devise', '~> 4.8', '>= 4.8.1'
 
-
+gem "bcrypt", "~> 3.1.7"
 
 group :development, :test do
   
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.precompile+=['custom.css']
+    config.assets.precompile+=%w(*.png *.jpg *.jpeg *.gif)
 
     # Configuration for the application, engines, and railties goes here.
     #
